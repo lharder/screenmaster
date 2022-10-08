f6b3d7044df8ddcba29eb7d9032399f788e6dede
lutz-harder <lutz.harder@freenet.ag>
2022-10-08 17:48:03 +0200

    Introduce overlay levels: freeze active level in time, load overlay level on top, remove and restart time for active level

--- screenmaster/screenmaster.lua
+++ screenmaster/screenmaster.lua
@@ -1,5 +1,25 @@
 local Screenmaster = {}
 
+local function unload( screenname )
+	if screenname then
+		url = screenname .. "#proxy"
+		msg.post( url, "release_input_focus" )
+		msg.post( url, "unload" ) 
+	end
+end
+
+
+local function sendInitMessages( messages )
+	if messages then 
+		timer.delay( 0.1, false, function()
+			for url, message in pairs( messages ) do
+				msg.post( url, "screenmaster:loaded", message )
+			end
+		end )
+	end
+end
+
+
 function Screenmaster.get()
 	if Screenmaster.inst == nil then
 		local this = {}
@@ -10,9 +30,7 @@ function Screenmaster.get()
 
 			-- unload the current level first
 			if this.screenname then
-				url = this.screenname .. "#proxy"
-				msg.post( url, "release_input_focus" )
-				msg.post( url, "unload" ) 
+				unload( this.screenname )
 			end
 
 			-- allow for unloading to complete: important
@@ -22,21 +40,45 @@ function Screenmaster.get()
 				url = screenname .. "#proxy"
 				msg.post( url, "load" )
 				this.screenname = screenname
-
+				
 				-- send user input to the new level
 				msg.post( url, "acquire_input_focus" )
 
 				-- allow for initialization messages to be  
 				-- sent after loading a new screen
-				if messages then 
-					timer.delay( 0.1, false, function()
-						for url, message in pairs( messages ) do
-							msg.post( url, "screenmaster:loaded", message )
-						end
-					end )
-				end
-				
-			end)
+				sendInitMessages( messages )			
+			end )
+		end
+
+
+		function this:loadOverlay( overlayname, messages )
+			-- unload the current level first
+			if this.screenname then
+				-- freeze time in active level
+				local url = msg.url( nil, this.screenname, "proxy" )
+				pprint( "Freeze " .. url )
+				msg.post( url, "set_time_step", { factor = 0, mode = 0 })
+			end
+
+			-- load the overlay on top of the frozen level
+			local url = overlayname .. "#proxy"
+			msg.post( url, "load" )
+			
+			-- send user input to the new level
+			msg.post( url, "acquire_input_focus" )
+
+			-- allow for initialization messages to be  
+			-- sent after loading a new screen
+			sendInitMessages( messages )
+		end
+
+
+		function this:removeOverlay( overlayname )
+			unload( overlayname )
+
+			local url = msg.url( nil, this.screenname, "proxy" )
+			pprint( "Unfreeze " .. url )
+			msg.post( url, "set_time_step", { factor = 1, mode = 0 })
 		end
 		
 
